f7fd8ab61d335770ccd76e2b7b29f09784e7240e
kocay <migokocay@gmail.com>
2026-03-12 21:40:57 +0100

    fix: update down_revision in migration script to correct reference

--- backend/alembic/versions/6f25fe56abef_add_geojson_file_url_to_projects.py
+++ backend/alembic/versions/6f25fe56abef_add_geojson_file_url_to_projects.py
@@ -1,7 +1,7 @@
 """add_geojson_file_url_to_projects
 
 Revision ID: 6f25fe56abef
-Revises: seed_admin_user
+Revises: 684589f25b1f
 Create Date: 2026-02-21 15:08:42.116702
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '6f25fe56abef'
-down_revision: Union[str, Sequence[str], None] = 'seed_admin_user'
+down_revision: Union[str, Sequence[str], None] = '684589f25b1f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
